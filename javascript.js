acff9311f009681e59e6512d543b23b39c4699db
Dexter Crago <jodieanddexter@gmail.com>
2023-11-25 11:03:42 -0700

    Use positioning to stop jerking behavior
    Whenever the '* Passwords do not match' message appeared it moved
    everything around in a very jerky manner. So i used absolute positioning
    to make it look better and added a min-height to the form so things
    don't get super weird if the browser is resized upwards.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
 let password1 = document.querySelector('#password1');
 let password2 = document.querySelector('#password2');
-let inputs = document.querySelector('.inputs');
+let errorMessageContainer = document.querySelector('.errorMessageContainer');
 
-let errorMessage = document.createElement('div');
+let errorMessage = document.createElement('p');
 errorMessage.style.cssText = 'color: red; font-size: 0.8rem;';
 errorMessage.textContent = '* Passwords do not match'
 
 password2.addEventListener('keyup', ()=> {
     if (password1.value === password2.value) {
-        inputs.removeChild(errorMessage);
+        errorMessageContainer.removeChild(errorMessage);
     } else {
-        inputs.appendChild(errorMessage);
+        errorMessageContainer.appendChild(errorMessage);
     }
 
 });
